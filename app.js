f5c3807673fc505959fa1e75ea5c49061ddb4075
yogesh <200345305001@paruluniversity.ac.in>
2025-02-03 11:59:32 +0530

    3rd Feb - added seperation of concerns in models and created owner model

---
+++ app.js
@@ -0,0 +1,29 @@
+const express =  require("express")
+const app = express();
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+const ownerRouter = require("./routes/ownerRouter");
+const userRouter = require("./routes/userRouter");
+const productsRouter = require("./routes/productsRouter");
+
+const db = require("./config/mongoose-connections");
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.use("/owner", ownerRouter);
+app.use("/user", userRouter);
+app.use("/products", productsRouter);
+
+//  app.get("/",(req,res)=>{
+//     res.send("hey");
+
+//  });
+  
+app.listen(3000); 
---
+++ models/owner-model.js
@@ -0,0 +1,19 @@
+const { type } = require('express/lib/response');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/backend project");
+
+const userSchema = mongoose.Schema({
+    fullname: String,
+    email : String,
+    password: String,
+    products: {
+        type:Array,
+        default: [],   
+    },
+    picture: String,
+    gstin: String,
+    contact: Number,
+    picture: String,
+});
+module.exports = mongoose.model("owner",ownerSchema);
